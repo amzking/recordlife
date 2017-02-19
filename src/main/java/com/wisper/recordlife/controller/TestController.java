161db815a9e2402e385426c1abd427da26fe0932
amzking <amzking@foxmail.com>
2017-02-19 08:55:12 +0800

    demo controller

---
+++ src/main/java/com/wisper/recordlife/controller/TestController.java
@@ -0,0 +1,11 @@
+package com.wisper.recordlife.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by zk on 17.2.19.
+ */
+@Controller
+public class TestController {
+
+}
